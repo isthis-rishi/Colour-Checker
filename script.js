da6c4a70815b3faf533fd18093400bfb883d005a
Rishi Kumar <125174773+isthis-rishi@users.noreply.github.com>
2024-01-07 18:50:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+        function changeColor() {
+            var color = document.getElementById('colorInput').value;
+            document.getElementById('textColor').style.backgroundColor = color;
+        }
